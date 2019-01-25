4db03f73f9744ddf3f388bc096fd9bc3cbf0999f
Harshita Agrawal <harshita.agrawal@somaiya.edu>
2019-01-25 18:41:16 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -7,28 +7,16 @@ const staticFiles = [
     './rules.html',
     './FAQs.html'
 ];
-var mode;
+var mode; //used to keep a check whether we are online at this point of time or offline
 
-// On installing the app, first time, cache all the staticFiles =>
-self.addEventListener("install", (e) => {
-    console.log("[Service Worker] Installed");
-    // wait untill event is complete =>
-    e.waitUntil(
-        caches.open("staticCache").then((cache) => {
-            console.log("Caching static files")
-            cache.addAll(staticFiles);
-        })
-    )
+// install event is called the very first time we register a servic worker
+self.addEventListener('install', async event => {
+    // #caches are http caches provided by service worker check Application >Cache > Cache Storage
+    const cache = await caches.open('static-def'); // stores the shell part
+    cache.addAll(staticAssets);
 });
 
-self.addEventListener('message', function(event){
-    if(event.data=="offline")
-    mode=false
-    else
-    mode=true
-    console.log("message: "+mode);
-});
-
+// called whenever fetxh function is called in the client side
 self.addEventListener('fetch', event => { 
     console.log(event);
     const {request} = event;
@@ -39,8 +27,36 @@ self.addEventListener('fetch', event => {
         if(url.origin === location.origin) { // if the url that we are searching for is in the domain check cache first
             event.respondWith(cacheData(request));
         } else {
-            //event.respondWith(networkFirst(request)); // for outside urls do not check cache 
+            event.respondWith(networkFirst(request)); // for outside urls do not check cache 
         }
     }
 
 });
+
+self.addEventListener('message', function(event){
+    if(event.data=="offline")
+    mode=false
+    else
+    mode=true
+    console.log("message: "+mode);
+});
+
+async function cacheData(request) 
+{
+    const cachedResponse = await caches.match(request); // check for request object for a match else returns nothing
+    return cachedResponse || fetch(request); 
+}
+
+async function networkFirst(request) 
+{
+    const cache = await caches.open('dynamic-def'); // stores the dynamic part of the pwa
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone()); // map new request objects to the result object
+        return response;
+    } catch (error){
+        return await cache.match(request); // if theres an error in making network first request it will check the caches
+    }
+
+}
